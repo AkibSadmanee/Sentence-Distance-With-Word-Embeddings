4c072cdea3f1b894668376feb7929773f212a67b
Akib Sadmanee <akib@agency.com>
2021-01-19 17:39:09 +0600

    initial commit

---
+++ embmeanloss.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy import spatial
+from gensim.models import Word2Vec
+
+model = Word2Vec.load("word2vec_skipgram_dim300_iter35.model")
+
+def meanloss(s1, s2):
+    sent1_vecs = []
+    sent2_vecs = []
+
+    for word in s1.split():
+        sent1_vecs.append(model.wv[word])
+        
+    for word in s2.split():
+        sent2_vecs.append(model.wv[word])
+
+    sent1_vecs = np.array(sent1_vecs)
+    mean1 = sent1_vecs.mean(axis=0)
+
+    sent2_vecs = np.array(sent2_vecs)
+    mean2 = sent2_vecs.mean(axis=0)
+    return (1 - spatial.distance.cosine(mean1, mean2))
---
+++ wmdloss.py
@@ -0,0 +1,8 @@
+import numpy as np
+from scipy import spatial
+from gensim.models import Word2Vec
+
+model = Word2Vec.load("word2vec_skipgram_dim300_iter35.model")
+
+def meanloss(s1, s2):
+    return (1 - model.wmdistance(s1, s2))
